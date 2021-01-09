0e0fd3dec0032c85614a76fcd9566ab078786db1
takasehideki <takase@i.kyoto-u.ac.jp>
2021-01-09 17:19:38 +0900

    add elixir-grpc to deps

--- mix.exs
+++ mix.exs
@@ -35,6 +35,8 @@ defmodule NervesGrpc.MixProject do
       {:shoehorn, "~> 0.7.0"},
       {:ring_logger, "~> 0.8.1"},
       {:toolshed, "~> 0.2.13"},
+      {:grpc, github: "elixir-grpc/grpc"},
+      {:cowlib, "~> 2.9.0", override: true},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
